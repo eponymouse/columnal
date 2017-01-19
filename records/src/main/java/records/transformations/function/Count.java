8786bd3a22d04f75d826230d657982af391e26ea
neil <neil@twistedsquare.com>
2017-01-19 08:40:31 +0000

    Implemented count, and test for it.

--- records/src/main/java/records/transformations/function/Count.java
+++ records/src/main/java/records/transformations/function/Count.java
@@ -1,5 +1,7 @@
 package records.transformations.function;
 
+import annotation.qual.Value;
+import com.google.common.collect.ImmutableList;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import records.data.datatype.DataType;
 import records.data.unit.Unit;
@@ -9,8 +11,11 @@ import records.error.UnimplementedException;
 import records.error.UserException;
 import records.transformations.expression.Expression;
 import records.transformations.expression.Expression._test_TypeVary;
+import threadchecker.OnThread;
+import threadchecker.Tag;
 import utility.ExConsumer;
 import utility.Pair;
+import utility.Utility;
 
 import java.util.Collections;
 import java.util.List;
@@ -29,12 +34,30 @@ public class Count extends FunctionDefinition
     @Override
     public @Nullable Pair<FunctionInstance, DataType> typeCheck(List<Unit> units, List<DataType> params, ExConsumer<String> onError, UnitManager mgr) throws InternalException, UserException
     {
-        throw new UnimplementedException();
+        if (!units.isEmpty())
+        {
+            onError.accept("Units should be empty");
+            return null;
+        }
+        if (params.size() != 1 || !params.get(0).isArray())
+        {
+            onError.accept("Count function accepts a single array parameter");
+        }
+        return new Pair<>(new Instance(), DataType.NUMBER);
     }
 
     @Override
     public Pair<List<Unit>, List<Expression>> _test_typeFailure(Random r, _test_TypeVary newExpressionOfDifferentType, UnitManager unitManager) throws UserException, InternalException
     {
         return new Pair<>(Collections.emptyList(), Collections.singletonList(newExpressionOfDifferentType.getDifferentType(DataType.array())));
+    }
+
+    private static class Instance extends FunctionInstance
+    {
+        @Override
+        public @OnThread(Tag.Simulation) @Value Object getValue(int rowIndex, ImmutableList<@Value Object> params) throws UserException, InternalException
+        {
+            return Utility.value(Utility.valueList(params.get(0)).size());
+        }
     }
 }
