dde6047ecba4118683f59d5a30d63ddbfdd543ce
neil <neil@twistedsquare.com>
2017-04-28 21:00:24 +0100

    Moved the annotations into their own maven sub-module.

--- records/annsrc/makejar.cmd
+++
@@ -1,6 +0,0 @@
-rmdir /s /q tmpout
-mkdir tmpout
-javac annotation/*.java annotation/qual/*.java annotation/userindex/*.java annotation/userindex/qual/*.java -cp C:\Users\neil\.m2\repository\org\checkerframework\checker\2.1.7\checker-2.1.7.jar -d tmpout
-cd tmpout
-jar cvf valueann.jar annotation
-copy valueann.jar ..\..\lib
