802f0f47dc5d4609b31b6d98a90cd64394e1d656
neil <neil@twistedsquare.com>
2017-08-31 00:39:15 +0100

    Swapped the expression editor test seed over to find a quicker test failure.

--- records/app/src/test/java/test/gui/TestExpressionEditor.java
+++ records/app/src/test/java/test/gui/TestExpressionEditor.java
@@ -67,7 +67,7 @@ public class TestExpressionEditor extends ApplicationTest implements ListUtilTra
     }
 
     @Property(trials = 10)
-    public void testEntry(@When(seed=-9954007651823638L) @From(GenExpressionValueForwards.class) @From(GenExpressionValueBackwards.class) ExpressionValue expressionValue, @When(seed=1L) @From(GenRandom.class) Random r) throws InterruptedException, ExecutionException, InternalException, IOException, UserException, InvocationTargetException
+    public void testEntry(@When(seed=1L) @From(GenExpressionValueForwards.class) @From(GenExpressionValueBackwards.class) ExpressionValue expressionValue, @When(seed=1L) @From(GenRandom.class) Random r) throws InterruptedException, ExecutionException, InternalException, IOException, UserException, InvocationTargetException
     {
         TestUtil.openDataAsTable(windowToUse, expressionValue.typeManager, expressionValue.recordSet);
         clickOn(".id-tableDisplay-menu-button").clickOn(".id-tableDisplay-menu-addTransformation");
