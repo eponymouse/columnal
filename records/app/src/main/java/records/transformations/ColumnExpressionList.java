69feadc2d37d21f0fb0a38a28e255efffb92ac4a
neil <neil@twistedsquare.com>
2017-09-10 11:11:26 +0100

    Added a context menu to the column expression list to allowing adding and deleting.

--- records/app/src/main/java/records/transformations/ColumnExpressionList.java
+++ records/app/src/main/java/records/transformations/ColumnExpressionList.java
@@ -5,7 +5,10 @@ import javafx.beans.binding.ObjectExpression;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -24,11 +27,14 @@ import records.data.datatype.DataType;
 import records.gui.ColumnNameTextField;
 import records.gui.SingleSourceControl;
 import records.gui.TypeLabel;
+import records.gui.expressioneditor.ErrorDisplayerRecord;
 import records.gui.expressioneditor.ExpressionEditor;
 import records.transformations.expression.Expression;
+import records.transformations.expression.UnfinishedExpression;
 import threadchecker.OnThread;
 import threadchecker.Tag;
 import utility.Pair;
+import utility.Utility;
 import utility.gui.FXUtility;
 import utility.gui.GUI;
 import utility.gui.ScrollPaneFill;
@@ -36,6 +42,9 @@ import utility.gui.ScrollPaneFill;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A GUI list of column names and expression editors for that column.
+ */
 @OnThread(Tag.FXPlatform)
 public class ColumnExpressionList
 {
@@ -43,44 +52,77 @@ public class ColumnExpressionList
     private final ScrollPaneFill columnListScrollPane;
     private SimpleBooleanProperty allColNamesValid = new SimpleBooleanProperty(false);
     private final BorderPane outerPane;
+    private final VBox columnEditors;
 
-    public ColumnExpressionList(TableManager mgr, SingleSourceControl srcTableControl, List<Pair<ColumnId, Expression>> initialColumns)
+    public ColumnExpressionList(TableManager mgr, SingleSourceControl srcTableControl, List<Pair<@Nullable ColumnId, Expression>> initialColumns)
     {
         ObjectExpression<@Nullable Table> srcTable = srcTableControl.tableProperty();
 
-        List<Node> columnEditors = new ArrayList<>();
-        for (Pair<ColumnId, Expression> newColumn : initialColumns)
+        columnEditors = new VBox();
+        for (Pair<@Nullable ColumnId, Expression> newColumn : initialColumns)
         {
-            SimpleObjectProperty<Expression> wrapper = new SimpleObjectProperty<>(newColumn.getSecond());
-            ColumnNameTextField columnNameTextField = new ColumnNameTextField(newColumn.getFirst()).withArrowLocation(ArrowLocation.BOTTOM_CENTER);
-            FXUtility.addChangeListenerPlatform(columnNameTextField.valueProperty(), v -> {
-                validateColumnNames();
-            });
-            this.columns.add(new Pair<ObjectExpression<@Nullable ColumnId>, ObjectExpression<Expression>>(columnNameTextField.valueProperty(), wrapper));
-            GridPane gridPane = new GridPane();
-            gridPane.add(columnNameTextField.getNode(), 0, 0);
-            ExpressionEditor expressionEditor = makeExpressionEditor(mgr, srcTable, wrapper);
-            TypeLabel typeLabel = new TypeLabel(expressionEditor.typeProperty());
-            gridPane.add(typeLabel, 1, 0);
-            GridPane.setHgrow(typeLabel, Priority.ALWAYS);
-            BorderPane containerAndLabel = new BorderPane(expressionEditor.getContainer(), null, null, null, new Label("="));
-            gridPane.add(containerAndLabel, 0, 1);
-            GridPane.setColumnSpan(containerAndLabel, 2);
-            GridPane.setHgrow(containerAndLabel, Priority.ALWAYS);
-            columnEditors.add(gridPane);
+            addColumn(mgr, srcTable, newColumn, columns.size());
         }
         columnListScrollPane = new ScrollPaneFill();
-        columnListScrollPane.setContent(new VBox(columnEditors.toArray(new Node[0])));
+        columnListScrollPane.setContent(columnEditors);
         validateColumnNames();
 
         outerPane = new BorderPane();
         BorderPane top = new BorderPane();
         top.setLeft(GUI.label("transformEditor.column.name"));
-        top.setRight(GUI.label("transformEditor.column.type"));
+        top.setCenter(GUI.label("transformEditor.column.type"));
         top.getStyleClass().add("column-expression-list-titles");
         outerPane.setTop(top);
         outerPane.setCenter(columnListScrollPane);
         outerPane.getStyleClass().add("column-expression-list");
+    }
+
+    @RequiresNonNull({"allColNamesValid", "columns", "columnEditors"})
+    private void addColumn(@UnknownInitialization(Object.class) ColumnExpressionList this,
+                           TableManager mgr, ObjectExpression<@Nullable Table> srcTable, Pair<@Nullable ColumnId, Expression> newColumn, int destIndex)
+    {
+        SimpleObjectProperty<Expression> wrapper = new SimpleObjectProperty<>(newColumn.getSecond());
+        ColumnNameTextField columnNameTextField = new ColumnNameTextField(newColumn.getFirst()).withArrowLocation(ArrowLocation.BOTTOM_CENTER);
+        FXUtility.addChangeListenerPlatform(columnNameTextField.valueProperty(), v -> {
+            validateColumnNames();
+        });
+        Pair<ObjectExpression<@Nullable ColumnId>, ObjectExpression<Expression>> columnPair = new Pair<ObjectExpression<@Nullable ColumnId>, ObjectExpression<Expression>>(columnNameTextField.valueProperty(), wrapper);
+        this.columns.add(destIndex, columnPair);
+        GridPane gridPane = new GridPane();
+        gridPane.add(columnNameTextField.getNode(), 0, 0);
+        ExpressionEditor expressionEditor = makeExpressionEditor(mgr, srcTable, wrapper);
+        TypeLabel typeLabel = new TypeLabel(expressionEditor.typeProperty());
+        gridPane.add(typeLabel, 1, 0);
+        GridPane.setHgrow(typeLabel, Priority.ALWAYS);
+        gridPane.add(GUI.buttonMenu("columnExpression.actions", () -> {
+            return new ContextMenu(
+                GUI.menuItem("columnExpression.delete", () -> {
+                    columns.remove(columnPair);
+                    columnEditors.getChildren().remove(gridPane);
+                }),
+                GUI.menuItem("columnExpression.copy", () -> {
+                    // Don't use destIndex, because it might be stale.  Instead find us in list:
+                    int curIndex = Utility.indexOfRef(columns, columnPair);
+                    addColumn(mgr, srcTable, new Pair<>(columnNameTextField.valueProperty().getValue(), expressionEditor.save(new ErrorDisplayerRecord(), err -> {})), curIndex + 1);
+                }),
+                GUI.menuItem("columnExpression.addBefore", () -> {
+                    // Don't use destIndex, because it might be stale.  Instead find us in list:
+                    int curIndex = Utility.indexOfRef(columns, columnPair);
+                    addColumn(mgr, srcTable, new Pair<>(null, new UnfinishedExpression("")), curIndex);
+                }),
+                GUI.menuItem("columnExpression.addAfter", () -> {
+                    // Don't use destIndex, because it might be stale.  Instead find us in list:
+                    int curIndex = Utility.indexOfRef(columns, columnPair);
+                    addColumn(mgr, srcTable, new Pair<>(null, new UnfinishedExpression("")), curIndex + 1);
+                })
+            );
+        }), 2, 0);
+
+        BorderPane containerAndLabel = new BorderPane(expressionEditor.getContainer(), null, null, null, new Label("="));
+        gridPane.add(containerAndLabel, 0, 1);
+        GridPane.setColumnSpan(containerAndLabel, 3);
+        GridPane.setHgrow(containerAndLabel, Priority.ALWAYS);
+        columnEditors.getChildren().add(destIndex, gridPane);
     }
 
 
@@ -88,6 +130,7 @@ public class ColumnExpressionList
     private void validateColumnNames(@UnknownInitialization ColumnExpressionList this)
     {
         allColNamesValid.set(columns.stream().allMatch((Pair<ObjectExpression<@Nullable ColumnId>, ObjectExpression<Expression>> p) -> p.getFirst().get() != null));
+        //TODO: also check for duplicates within this list
     }
 
 
