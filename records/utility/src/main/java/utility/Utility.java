09527e1445c01c5ba9ecbf6392c88128d09abc49
neil <neil@twistedsquare.com>
2017-10-21 16:47:37 +0100

    Missing import from last commit

--- records/utility/src/main/java/utility/Utility.java
+++ records/utility/src/main/java/utility/Utility.java
@@ -18,6 +18,7 @@ import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
