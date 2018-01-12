c3f48b9d15f88864bb2c6aaf7c8df858713b9d61
neil <neil@twistedsquare.com>
2018-01-12 15:55:47 +0000

    Fixed some display aspects of the initial window

--- records/app/src/main/java/records/gui/InitialWindow.java
+++ records/app/src/main/java/records/gui/InitialWindow.java
@@ -1,5 +1,7 @@
 package records.gui;
 
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
@@ -8,6 +10,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import records.error.InternalException;
 import records.error.UserException;
@@ -19,6 +22,7 @@ import utility.gui.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by neil on 17/04/2017.
@@ -54,6 +58,8 @@ public class InitialWindow
             stage.hide();
         });
         ListView<File> mruListView = new ListView<>();
+        mruListView.setPrefHeight(200.0);
+        mruListView.setMaxHeight(Double.MAX_VALUE);
         mruListView.setOnMouseClicked(e -> {
             if (e.getButton() == MouseButton.PRIMARY && e.getClickCount() == 2)
             {
@@ -66,14 +72,26 @@ public class InitialWindow
         });
         mruListView.getItems().setAll(Utility.readRecentFilesList());
         VBox content = GUI.vbox("initial-content",
-                GUI.vbox("initial-section-new", GUI.label("initial.new.title", "initial-heading"), newButton, GUI.labelWrap("initial.new.detail")),
-                GUI.vbox("initial-section-open", GUI.label("initial.open.title", "initial-heading"), openButton, GUI.label("initial.open.recent"), mruListView)
+                headed("initial-section-new", GUI.label("initial.new.title", "initial-heading"), newButton, GUI.labelWrap("initial.new.detail")),
+                headed("initial-section-open", GUI.label("initial.open.title", "initial-heading"), openButton, GUI.vbox("initial-recent", GUI.label("initial.open.recent", "initial-subheading"), mruListView))
         );
         Scene scene = new Scene(new BorderPane(content, menuBar, null, null, null));
         scene.getStylesheets().addAll(FXUtility.getSceneStylesheets("initial"));
         stage.setScene(scene);
         stage.show();
         //org.scenicview.ScenicView.show(scene);
+    }
+
+    private static Node headed(String style, Label header, Node... content)
+    {
+        Node[] all = new Node[content.length + 1];
+        all[0] = header;
+        for (int i = 0; i < content.length; i++)
+        {
+            VBox.setMargin(content[i], new Insets(0, 0, 0, 30));
+            all[i + 1] = content[i];
+        }
+        return GUI.vbox(style, all);
     }
 
     public static void chooseAndOpenProject(Stage parent)
