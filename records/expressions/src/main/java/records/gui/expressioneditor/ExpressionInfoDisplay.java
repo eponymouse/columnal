79251320e286e1ba5ab448f81e65edf10b19a697
neil <neil@twistedsquare.com>
2018-01-12 21:57:25 +0000

    Added a slow hide animation when mouse leaves an ExpressionInfoDisplay-associated item, so that the user has a chance to hover over it (which cancels the hide) or detach it.

--- records/expressions/src/main/java/records/gui/expressioneditor/ExpressionInfoDisplay.java
+++ records/expressions/src/main/java/records/gui/expressioneditor/ExpressionInfoDisplay.java
@@ -1,5 +1,9 @@
 package records.gui.expressioneditor;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.SimpleStringProperty;
@@ -13,6 +17,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
+import org.checkerframework.checker.nullness.qual.EnsuresNonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.checker.nullness.qual.RequiresNonNull;
 import org.controlsfx.control.PopOver;
@@ -32,6 +39,10 @@ import java.util.List;
  *  - (The user hovers over the main part or focuses the main part) AND there is an error.
  * Bindings to do this will get too intricate, so we just keep track of the properties involved
  *  
+ * Additionally, when we should hide, but there is an error showing, we hide slowly, so the user gets
+ * time to mouse-over us to access the fixes and so on.  If the user mouses-over us, we cancel any current
+ * hide.
+ *  
  */
 public class ExpressionInfoDisplay
 {
@@ -41,17 +52,25 @@ public class ExpressionInfoDisplay
     private final VBox expressionNode;
     private @Nullable PopOver popup = null;
     private boolean focused = false;
-    private boolean hoveringOverall = false;
-    private boolean hoveringTop = false;
+    private boolean hoveringAttached = false;
+    private boolean hoveringTopOfAttached = false;
+    private boolean hoveringPopup = false;
+    private @Nullable Animation hidingAnimation;
 
     @SuppressWarnings("initialization")
     public ExpressionInfoDisplay(VBox expressionNode, Label topLabel, TextField textField)
     {
         this.expressionNode = expressionNode;
         FXUtility.addChangeListenerPlatformNN(textField.focusedProperty(), this::textFieldFocusChanged);
-        FXUtility.addChangeListenerPlatformNN(expressionNode.hoverProperty(), b -> mouseHoverStatusChanged(expressionNode.isHover(), topLabel.isHover()));
-        FXUtility.addChangeListenerPlatformNN(topLabel.hoverProperty(), b -> mouseHoverStatusChanged(expressionNode.isHover(), topLabel.isHover()));
-        FXUtility.addChangeListenerPlatformNN(errorMessage, s -> updateShowHide());
+        FXUtility.addChangeListenerPlatformNN(expressionNode.hoverProperty(), b -> {
+            hoveringAttached = b;
+            mouseHoverStatusChanged();
+        });
+        FXUtility.addChangeListenerPlatformNN(topLabel.hoverProperty(), b -> {
+            hoveringTopOfAttached = b;
+            mouseHoverStatusChanged();
+        });
+        FXUtility.addChangeListenerPlatformNN(errorMessage, s -> updateShowHide(true));
     }
 
     private class ErrorMessagePopup extends PopOver
@@ -60,6 +79,7 @@ public class ExpressionInfoDisplay
 
         public ErrorMessagePopup()
         {
+            setDetachable(true);
             getStyleClass().add("expression-info-popup");
             setArrowLocation(ArrowLocation.BOTTOM_CENTER);
             // If we let the position vary to fit on screen, we end up with the popup bouncing in and out
@@ -84,7 +104,7 @@ public class ExpressionInfoDisplay
             ListView<QuickFix> fixList = new ListView<>(quickFixes);
             fixList.setMaxHeight(150.0);
             // Keep reference to prevent GC:
-            hasFixes = Bindings.isEmpty(quickFixes).not();
+            hasFixes = Bindings.isNotEmpty(quickFixes);
             fixList.visibleProperty().bind(hasFixes);
             fixList.managedProperty().bind(fixList.visibleProperty());
 
@@ -94,7 +114,7 @@ public class ExpressionInfoDisplay
                 protected void updateItem(@Nullable QuickFix item, boolean empty) {
                     super.updateItem(item, empty);
                     setText("");
-                    setGraphic(empty || item == null ? null : new TextFlow(new Text(item.getTitle())));
+                    setGraphic((empty || item == null) ? null : new TextFlow(new Text(item.getTitle())));
                 }
             });
 
@@ -104,6 +124,11 @@ public class ExpressionInfoDisplay
             BorderPane container = new BorderPane(errorLabel, typeLabel, null, fixList, null);
 
             setContentNode(container);
+            FXUtility.addChangeListenerPlatformNN(getRoot().hoverProperty(), b -> {
+                hoveringPopup = b;
+                mouseHoverStatusChanged();
+            });
+            FXUtility.addChangeListenerPlatformNN(detachedProperty(), b -> updateShowHide(false));
         }
     }
 
@@ -112,7 +137,7 @@ public class ExpressionInfoDisplay
         // Shouldn't be non-null already, but just in case:
         if (popup != null)
         {
-            hide();
+            hide(true);
         }
         if (expressionNode.getScene() != null)
         {
@@ -123,37 +148,79 @@ public class ExpressionInfoDisplay
 
     @RequiresNonNull("popup")
     // Can't have an ensuresnull check
-    private void hide()
+    private void hide(boolean immediately)
     {
-        popup.hide();
-        popup = null;
+        @NonNull PopOver popupFinal = popup;
+        // Whether we hide immediately or not, stop any current animation:
+        cancelHideAnimation();
+
+        if (immediately)
+        {
+            popupFinal.hide();
+            popup = null;
+        }
+        else
+        {
+            
+            hidingAnimation = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(popupFinal.opacityProperty(), 1.0)),
+                new KeyFrame(Duration.millis(2000), new KeyValue(popupFinal.opacityProperty(), 0.0))
+            );
+            hidingAnimation.setOnFinished(e -> {
+                if (popup != null)
+                {
+                    popup.hide();
+                    popup = null;
+                }
+            });
+            hidingAnimation.playFromStart();
+        }
     }
 
-    private void mouseHoverStatusChanged(boolean hoverOverall, boolean hoverTop)
+    private void mouseHoverStatusChanged()
     {
-        this.hoveringOverall = hoverOverall;
-        this.hoveringTop = hoverTop;
-        updateShowHide();
+        updateShowHide(false);
     }
 
     private void textFieldFocusChanged(boolean newFocused)
     {
         this.focused = newFocused;
-        updateShowHide();
+        updateShowHide(true);
     }
 
 
-    private void updateShowHide()
+    private void updateShowHide(boolean hideImmediately)
     {
-        if (hoveringTop || ((hoveringOverall || focused) && !errorMessage.get().isEmpty()))
+        if (hoveringPopup || hoveringTopOfAttached || ((hoveringAttached || focused) && !errorMessage.get().isEmpty()) || (popup != null && popup.isDetached()))
         {
             if (popup == null)
+            {
                 show();
+            }
+            else
+            {
+                // Make sure to cancel any hide animation:
+                cancelHideAnimation();
+            }
         }
         else
         {
             if (popup != null)
-                hide();
+                hide(hideImmediately);
+        }
+    }
+
+    //@EnsuresNull("hidingAnimation")
+    private void cancelHideAnimation()
+    {        
+        if (hidingAnimation != null)
+        {
+            hidingAnimation.stop();
+            hidingAnimation = null;
+        }
+        if (popup != null)
+        {
+            popup.setOpacity(1.0);
         }
     }
 
@@ -166,7 +233,7 @@ public class ExpressionInfoDisplay
             // Hide the popup:
             if (popup != null)
             {
-                hide();
+                hide(true);
             }
         }
         else
