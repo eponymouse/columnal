da994124ff0ad86a7f0d03f8c3b00acaed407c0c
neil <neil@twistedsquare.com>
2018-04-05 21:29:01 +0100

    Improved the direction button in the sort-edit dialog.

--- records/app/src/main/java/records/gui/EditSortDialog.java
+++ records/app/src/main/java/records/gui/EditSortDialog.java
@@ -13,14 +13,23 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
 import javafx.stage.Modality;
 import log.Log;
 import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import records.data.Column;
 import records.data.ColumnId;
+import records.data.RecordSet;
 import records.data.Table;
 import records.data.TableId;
+import records.data.datatype.DataType;
+import records.data.datatype.DataType.DataTypeVisitor;
+import records.data.datatype.DataType.DateTimeInfo;
+import records.data.datatype.DataType.TagType;
+import records.data.datatype.NumberInfo;
+import records.data.datatype.TypeId;
 import records.error.InternalException;
 import records.error.UserException;
 import records.gui.expressioneditor.AutoComplete;
@@ -31,6 +40,7 @@ import records.transformations.Sort.Direction;
 import threadchecker.OnThread;
 import threadchecker.Tag;
 import utility.Pair;
+import utility.UnitType;
 import utility.gui.FXUtility;
 import utility.gui.FancyList;
 import utility.gui.LightDialog;
@@ -42,12 +52,23 @@ import java.util.stream.Stream;
 public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Direction>>>
 {
     private final ImmutableList<Table> possibleTables;
+    private final @Nullable RecordSet dataWithColumns;
 
     public EditSortDialog(View parent, Point2D lastScreenPos, @Nullable Table srcTable, Table destTable, ImmutableList<Pair<ColumnId, Direction>> originalSortBy)
     {
         super(parent.getWindow());
         initModality(Modality.NONE);
         possibleTables = srcTable == null ? ImmutableList.of(destTable) : ImmutableList.of(srcTable, destTable);
+        @Nullable RecordSet d = null;
+        try
+        {
+            d = srcTable == null ? destTable.getData() : srcTable.getData();
+        }
+        catch (InternalException | UserException e)
+        {
+            Log.log(e);
+        }
+        dataWithColumns = d;
 
         SortList sortList = new SortList(originalSortBy);
         getDialogPane().setContent(sortList.getNode());
@@ -77,6 +98,7 @@ public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Dir
         });
     }
 
+    @OnThread(Tag.FXPlatform)
     private class SortList extends FancyList<Pair<ColumnId, Direction>, SortPane>
     {
         public SortList(ImmutableList<Pair<ColumnId, Direction>> initialItems)
@@ -93,7 +115,26 @@ public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Dir
 
         public void pickColumnIfEditing(Pair<Table, ColumnId> t)
         {
-            // TODO
+            // This is a bit of a hack.  The problem is that clicking the column removes the focus
+            // from the edit field, so we can't just ask if the edit field is focused.  Tracking who
+            // the focus transfers from/to seems a bit awkward, so we just use a time-based system,
+            // where if they were very recently (200ms) editing a field, fill in that field with the table.
+            // If they weren't recently editing a field, we append to the end of the list.
+            long curTime = System.currentTimeMillis();
+            SortPane curEditing = streamCells()
+                    .map(cell -> cell.getContent())
+                    .filter(p -> p.lastEditTimeMillis() > curTime - 200L).findFirst().orElse(null);
+            if (curEditing != null)
+            {
+                curEditing.setContent(t.getSecond());
+                if (addButton != null)
+                    addButton.requestFocus();
+            }
+            else
+            {
+                // Add to end:
+                addToEnd(new Pair<>(t.getSecond(), Direction.ASCENDING), false);
+            }
         }
     }
 
@@ -102,12 +143,15 @@ public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Dir
     {
         private final SimpleObjectProperty<Pair<ColumnId, Direction>> currentValue;
         private final TextField columnField;
+        private final AutoComplete autoComplete;
+        private final DirectionButton button;
+        private long lastEditTimeMillis = -1;
 
         public SortPane(@Nullable Pair<ColumnId, Direction> initialContent)
         {
             currentValue = new SimpleObjectProperty<>(initialContent == null ? new Pair<>(new ColumnId(""), Direction.ASCENDING) : initialContent);
             columnField = new TextField();
-            new AutoComplete(columnField,
+            autoComplete = new AutoComplete(columnField,
                 (s, q) -> possibleTables.stream().flatMap(t -> {
                     try
                     {
@@ -120,13 +164,22 @@ public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Dir
                     }
                 }).filter(c -> c.getName().getOutput().contains(s)).map(ColumnCompletion::new).collect(Collectors.<Completion>toList()),
                 getListener(), WhitespacePolicy.ALLOW_ONE_ANYWHERE_TRIM, c -> false);
+            FXUtility.addChangeListenerPlatformNN(columnField.focusedProperty(), focus -> {
+                // Update whether focus is arriving or leaving:
+                lastEditTimeMillis = System.currentTimeMillis();
+            });
             setTop(new Label("Type table name or click on column"));
             setCenter(columnField);
-            BorderPane sortGraphic = new BorderPane();
-            sortGraphic.setTop(new Label("A"));
-            sortGraphic.setBottom(new Label("Z"));
-            setRight(new Button("\u2191", sortGraphic));
+            button = new DirectionButton();
+            button.setDirection(initialContent == null ? Direction.ASCENDING : initialContent.getSecond());
+            button.setType(calculateTypeOf(initialContent == null ? null : initialContent.getFirst()));
+            setRight(button);
             
+        }
+
+        public long lastEditTimeMillis()
+        {
+            return columnField.isFocused() ? System.currentTimeMillis() : lastEditTimeMillis;
         }
 
         private CompletionListener getListener(@UnknownInitialization SortPane this)
@@ -169,12 +222,188 @@ public class EditSortDialog extends LightDialog<ImmutableList<Pair<ColumnId, Dir
             };
         }
 
+        public void setContent(ColumnId columnId)
+        {
+            autoComplete.setContentDirect(columnId.getRaw());
+            currentValue.set(new Pair<>(columnId, currentValue.get().getSecond()));
+        }
+
         public ObjectExpression<Pair<ColumnId, Direction>> currentValue()
         {
             return currentValue;
         }
+
+        private class DirectionButton extends Button
+        {
+            private final Label topLabel;
+            private final Label bottomLabel;
+            private final Shape icon;
+            private String smallItem = "A";
+            private String largeItem = "Z";
+            private Direction direction = Direction.ASCENDING;
+
+            public DirectionButton()
+            {
+                icon = new Polygon(5, 0, 10, 25, 0, 25);
+                BorderPane sortGraphic = new BorderPane();
+                topLabel = new Label(smallItem);
+                bottomLabel = new Label(largeItem);
+                sortGraphic.setCenter(new BorderPane(null, topLabel, null, bottomLabel, null));
+                sortGraphic.setRight(icon);
+                setText("");
+                setGraphic(sortGraphic);
+                setOnAction(e -> {
+                    FXUtility.mouse(this).setDirection(direction == Direction.ASCENDING ? Direction.DESCENDING : Direction.ASCENDING);
+                });
+            }
+
+            public void setDirection(Direction direction)
+            {
+                this.direction = direction;
+                icon.setRotate(direction == Direction.ASCENDING ? 0 : 180);
+                topLabel.setText(direction == Direction.ASCENDING ? smallItem : largeItem);
+                bottomLabel.setText(direction == Direction.ASCENDING ? largeItem : smallItem);
+            }
+            
+            public void setType(@Nullable DataType dataType)
+            {
+                if (dataType == null)
+                    dataType = DataType.TEXT;
+                try
+                {
+                    dataType.apply(new DataTypeVisitor<UnitType>()
+                    {
+                        @Override
+                        public UnitType number(NumberInfo numberInfo) throws InternalException, UserException
+                        {
+                            smallItem = "1";
+                            largeItem = "99";
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType text() throws InternalException, UserException
+                        {
+                            smallItem = "A";
+                            largeItem = "Z";
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType date(DateTimeInfo dateTimeInfo) throws InternalException, UserException
+                        {
+                            switch (dateTimeInfo.getType())
+                            {
+                                case YEARMONTHDAY:
+                                case YEARMONTH:
+                                case DATETIME:
+                                case DATETIMEZONED:
+                                    smallItem = "1965";
+                                    largeItem = "2016";
+                                    break;
+                                case TIMEOFDAY:
+                                case TIMEOFDAYZONED:
+                                    smallItem = "00:00";
+                                    largeItem = "11:21";
+                                    break;
+                                
+                            }
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType bool() throws InternalException, UserException
+                        {
+                            smallItem = "false";
+                            largeItem = "true";
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType tagged(TypeId typeName, ImmutableList<DataType> typeVars, ImmutableList<TagType<DataType>> tags) throws InternalException, UserException
+                        {
+                            if (tags.size() == 1 && tags.get(0).getInner() != null)
+                            {
+                                // Recurse first:
+                                tags.get(0).getInner().apply(this);
+                                // Then wrap:
+                                smallItem = tags.get(0).getName() + " (" + smallItem + ")";
+                                largeItem = tags.get(0).getName() + " (" + largeItem + ")";
+                            }
+                            else
+                            {
+                                // Just show tag names:
+                                smallItem = tags.get(0).getName() + " \u2026";
+                                largeItem = tags.get(tags.size() - 1).getName() + " \u2026";
+                            }
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType tuple(ImmutableList<DataType> inner) throws InternalException, UserException
+                        {
+                            // Recurse first:
+                            inner.get(0).apply(this);
+                            // Then wrap:
+                            smallItem = "(" + smallItem + ", \u2026)";
+                            largeItem = "(" + largeItem + ", \u2026)";
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType array(@Nullable DataType inner) throws InternalException, UserException
+                        {
+                            if (inner == null)
+                            {
+                                smallItem = "[]";
+                                largeItem = "[]";
+                            }
+                            else
+                            {
+                                // Recurse first:
+                                inner.apply(this);
+                                // Then wrap:
+                                smallItem = "[" + smallItem + ", \u2026]";
+                                largeItem = "[" + largeItem + ", \u2026]";
+                            }
+                            return UnitType.UNIT;
+                        }
+
+                        @Override
+                        public UnitType toInfer() throws InternalException, UserException
+                        {
+                            return text();
+                        }
+                    });
+                }
+                catch (UserException | InternalException e)
+                {
+                    Log.log(e);
+                }
+                // Will update labels:
+                setDirection(direction);
+            }
+        }
     }
-    
+
+    private @Nullable DataType calculateTypeOf(@Nullable ColumnId columnId)
+    {
+        if (columnId == null || dataWithColumns == null)
+            return null;
+        @Nullable Column c = dataWithColumns.getColumnOrNull(columnId);
+        if (c == null)
+            return null;
+        try
+        {
+            return c.getType();
+        }
+        catch (InternalException | UserException e)
+        {
+            Log.log(e);
+            return null;
+        }
+    }
+
     private static class ColumnCompletion extends Completion
     {
         private final Column c;
