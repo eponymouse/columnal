46a2da42682e6a6353eeec2df21aeb3e84bfb092
neil <neil@twistedsquare.com>
2018-05-26 19:32:58 +0100

    Added limited use of pattern match variables to expression generation.

--- records/app/src/test/java/test/gen/backwards/BackwardsMatch.java
+++ records/app/src/test/java/test/gen/backwards/BackwardsMatch.java
@@ -6,14 +6,20 @@ import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import records.data.datatype.DataType;
+import records.data.datatype.DataType.DataTypeVisitor;
+import records.data.datatype.DataType.DateTimeInfo;
 import records.data.datatype.DataType.SpecificDataTypeVisitor;
 import records.data.datatype.DataType.TagType;
+import records.data.datatype.DataTypeUtility;
+import records.data.datatype.NumberInfo;
 import records.data.datatype.TaggedTypeDefinition;
 import records.data.datatype.TypeId;
 import records.data.datatype.TypeManager.TagInfo;
 import records.data.unit.Unit;
 import records.error.InternalException;
 import records.error.UserException;
+import records.transformations.expression.AddSubtractExpression;
+import records.transformations.expression.AddSubtractExpression.Op;
 import records.transformations.expression.AndExpression;
 import records.transformations.expression.EqualExpression;
 import records.transformations.expression.Expression;
@@ -21,6 +27,8 @@ import records.transformations.expression.IdentExpression;
 import records.transformations.expression.MatchExpression;
 import records.transformations.expression.MatchExpression.MatchClause;
 import records.transformations.expression.MatchExpression.Pattern;
+import records.transformations.expression.NumericLiteral;
+import records.transformations.expression.TimesExpression;
 import records.transformations.expression.VarDeclExpression;
 import test.DummyManager;
 import test.TestUtil;
@@ -36,25 +44,123 @@ import utility.Utility;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BackwardsMatch extends BackwardsProvider
 {
+    private static class VarInfo
+    {
+        private final String name;
+        private final DataType type;
+        private final Object value;
+
+        public VarInfo(String name, DataType type, Object value)
+        {
+            this.name = name;
+            this.type = type;
+            this.value = value;
+        }
+    }
+    
     private int nextVar = 0;
+    private ArrayList<ArrayList<VarInfo>> varContexts = new ArrayList<>();
 
     public BackwardsMatch(SourceOfRandomness r, RequestBackwardsExpression parent)
     {
         super(r, parent);
     }
+    
+    private @Nullable VarInfo findVarOfType(Predicate<DataType> typePred)
+    {
+        ArrayList<VarInfo> possibles = new ArrayList<>();
+        for (ArrayList<VarInfo> varContext : varContexts)
+        {
+            for (VarInfo varInfo : varContext)
+            {
+                if (typePred.test(varInfo.type))
+                {
+                    possibles.add(varInfo); 
+                }
+            }
+        }
+        return possibles.isEmpty() ? null : r.choose(possibles);
+    }
 
     @Override
     public List<ExpressionMaker> terminals(DataType targetType, @Value Object targetValue) throws InternalException, UserException
     {
-        return ImmutableList.of();
+        // For temporal, numerical, boolean and text, we manipulate them to be what we want.
+        // For boolean, we may use an equals expression.
+        
+        return targetType.apply(new DataTypeVisitor<List<ExpressionMaker>>()
+        {
+            @Override
+            public List<ExpressionMaker> number(NumberInfo numberInfo) throws InternalException, UserException
+            {
+                @Nullable VarInfo numVar = findVarOfType(DataType::isNumber);
+                if (numVar == null)
+                    return ImmutableList.of();
+                @NonNull VarInfo numVarFinal = numVar;
+                IdentExpression varRef = new IdentExpression(numVar.name);
+                return ImmutableList.of(() -> {
+                    return new AddSubtractExpression(ImmutableList.of(
+                        new TimesExpression(ImmutableList.of(varRef, new NumericLiteral(1, parent.makeUnitExpression(numberInfo.getUnit().divideBy(numVarFinal.type.getNumberInfo().getUnit()))))),
+                            new NumericLiteral(Utility.addSubtractNumbers((Number)targetValue, (Number)numVarFinal.value, false), parent.makeUnitExpression(numberInfo.getUnit()))
+                    ), ImmutableList.of(Op.SUBTRACT));
+                });
+            }
+
+            @Override
+            public List<ExpressionMaker> text() throws InternalException, UserException
+            {
+                // TODO include text replace
+                return ImmutableList.of();
+            }
+
+            @Override
+            public List<ExpressionMaker> date(DateTimeInfo dateTimeInfo) throws InternalException, UserException
+            {
+                // TODO include date manipulation
+                return ImmutableList.of();
+            }
+
+            @Override
+            public List<ExpressionMaker> bool() throws InternalException, UserException
+            {
+                @Nullable VarInfo boolVar = findVarOfType(t -> t.equals(DataType.BOOLEAN));
+                if (boolVar == null)
+                    return ImmutableList.of();
+                else
+                {
+                    String name = boolVar.name;
+                    return ImmutableList.of(() -> new IdentExpression(name));
+                }
+            }
+
+            @Override
+            public List<ExpressionMaker> tagged(TypeId typeName, ImmutableList<Either<Unit, DataType>> typeVars, ImmutableList<TagType<DataType>> tags) throws InternalException, UserException
+            {
+                return ImmutableList.of();
+            }
+
+            @Override
+            public List<ExpressionMaker> tuple(ImmutableList<DataType> inner) throws InternalException, UserException
+            {
+                return ImmutableList.of();
+            }
+
+            @Override
+            public List<ExpressionMaker> array(DataType inner) throws InternalException, UserException
+            {
+                return ImmutableList.of();
+            }
+        });
     }
 
     @Override
@@ -93,9 +199,12 @@ public class BackwardsMatch extends BackwardsProvider
                 }
             });
         }).stream().<Function<MatchExpression, MatchClause>>flatMap(o -> o.isPresent() ? Stream.<Function<MatchExpression, MatchClause>>of(o.get()) : Stream.<Function<MatchExpression, MatchClause>>empty()).collect(Collectors.<Function<MatchExpression, MatchClause>>toList()));
-        Expression correctOutcome = parent.make(targetType, targetValue, maxLevels - 1);
         List<ExFunction<MatchExpression, Pattern>> patterns = new ArrayList<>(makeNonMatchingPatterns(maxLevels - 1, t, actual));
+        
+        // Add var context for successful pattern:
+        varContexts.add(new ArrayList<>());
         Pair<Expression, @Nullable Expression> match = makePatternMatch(maxLevels - 1, t, actual);
+        Expression correctOutcome = parent.make(targetType, targetValue, maxLevels - 1);
         patterns.add(r.nextInt(0, patterns.size()), me -> {
             @Nullable Expression guard = r.nextBoolean() ? null : parent.make(DataType.BOOLEAN, true, maxLevels - 1);
             @Nullable Expression extraGuard = match.getSecond();
@@ -103,6 +212,8 @@ public class BackwardsMatch extends BackwardsProvider
                 guard = (guard == null ? extraGuard : new AndExpression(Arrays.asList(guard, extraGuard)));
             return new Pattern(match.getFirst(), guard);
         });
+        // Remove for successful pattern:
+        varContexts.remove(varContexts.size() -1);
         clauses.add(r.nextInt(0, clauses.size()), me -> {
             try
             {
@@ -151,6 +262,8 @@ public class BackwardsMatch extends BackwardsProvider
             else if (r.nextBoolean()) // Do equals but using variable + guard
             {
                 String varName = "var" + nextVar++;
+                if (!varContexts.isEmpty())
+                    varContexts.get(varContexts.size() - 1).add(new VarInfo(varName, t, actual));
                 return new Pair<>(new VarDeclExpression(varName), new EqualExpression(ImmutableList.of(new IdentExpression(varName), parent.make(t, actual, maxLevels))));
             }
             Expression expression = parent.make(t, actual, maxLevels);
