8ff5c21b1866844bb98093a6245aa01024d454a3
neil <neil@twistedsquare.com>
2019-01-05 22:09:11 +0000

    Fixed yet another type test manager issue, and made sure that if the type check fails, an error exists.

--- records/app/src/test/java/test/expressions/PropRunExpression.java
+++ records/app/src/test/java/test/expressions/PropRunExpression.java
@@ -5,12 +5,15 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.When;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.runner.RunWith;
 import records.error.InternalException;
 import records.error.UserException;
 import records.transformations.expression.ErrorAndTypeRecorderStorer;
 import records.transformations.expression.EvaluateState;
+import records.transformations.expression.Expression.CheckedExp;
 import records.transformations.expression.Expression.LocationInfo;
+import records.transformations.expression.TypeState;
 import test.DummyManager;
 import test.TestUtil;
 import test.gen.ExpressionValue;
@@ -25,8 +28,7 @@ import java.util.List;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * Created by neil on 10/12/2016.
@@ -36,17 +38,19 @@ public class PropRunExpression
 {
     @Property(trials = 2000)
     @OnThread(Tag.Simulation)
-    public void propRunExpression(
+    public void propRunExpression(@When(seed=-4106717301500151989L)
             @From(GenExpressionValueBackwards.class) @From(GenExpressionValueForwards.class) ExpressionValue src) throws InternalException, UserException
     {
         try
         {
+            DummyManager dummyManager = TestUtil.managerWithTestTypes().getFirst();
             ErrorAndTypeRecorderStorer errors = new ErrorAndTypeRecorderStorer();
-            src.expression.check(src, TestUtil.typeState(), LocationInfo.UNIT_DEFAULT, errors);
+            @Nullable CheckedExp checked = src.expression.check(src, new TypeState(dummyManager.getUnitManager(), dummyManager.getTypeManager()), LocationInfo.UNIT_DEFAULT, errors);
+            assertEquals("Checked iff error", checked == null, errors.getAllErrors().count() != 0);
             errors.withFirst(s -> {throw new InternalException(s.toPlain());});
             for (int row = 0; row < src.value.size(); row++)
             {
-                @Value Object actualValue = src.expression.getValue(new EvaluateState(DummyManager.make().getTypeManager(), OptionalInt.of(row))).getFirst();
+                @Value Object actualValue = src.expression.getValue(new EvaluateState(dummyManager.getTypeManager(), OptionalInt.of(row))).getFirst();
                 assertTrue("{{{" + src.expression.toString() + "}}} should have been " + TestUtil.toString(src.value.get(row)) + " but was " + TestUtil.toString(actualValue) + " columns: " + src.recordSet.getColumnIds().stream().map(Object::toString).collect(Collectors.joining(", ")) + " " + src.recordSet.debugGetVals(row),
                     Utility.compareValues(src.value.get(row), actualValue, new BigDecimal("0.000000001")) == 0);
             }
