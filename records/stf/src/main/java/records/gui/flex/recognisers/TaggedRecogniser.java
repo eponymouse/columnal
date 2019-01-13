036f1ca480aeadbff3e5608594984f9d7b027230
neil <neil@twistedsquare.com>
2019-01-13 11:02:18 +0000

    Fixed a bug in the tag recogniser when one tag is an exact prefix of another, and earlier in the list.

--- records/stf/src/main/java/records/gui/flex/recognisers/TaggedRecogniser.java
+++ records/stf/src/main/java/records/gui/flex/recognisers/TaggedRecogniser.java
@@ -6,7 +6,15 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import records.data.datatype.DataType.TagType;
 import records.gui.flex.Recogniser;
 import utility.Either;
+import utility.Pair;
 import utility.TaggedValue;
+import utility.Utility;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaggedRecogniser extends Recogniser<@Value TaggedValue>
 {
@@ -21,16 +29,20 @@ public class TaggedRecogniser extends Recogniser<@Value TaggedValue>
     public Either<ErrorDetails, SuccessDetails<TaggedValue>> process(ParseProgress parseProgress, boolean immediatelySurroundedByRoundBrackets)
     {
         ParseProgress pp = parseProgress.skipSpaces();
-        for (int i = 0; i < tags.size(); i++)
+        
+        List<Pair<Integer, TagType<Recogniser<@Value ?>>>> tagsLargestFirst = new ArrayList<>(Utility.streamIndexed(tags).collect(Collectors.<Pair<Integer, TagType<Recogniser<@Value ?>>>>toList()));
+        // Longest names first:
+        Collections.<Pair<Integer, TagType<Recogniser<@Value ?>>>>sort(tagsLargestFirst, Comparator.<Pair<Integer, TagType<Recogniser<@Value ?>>>, Integer>comparing(p -> -p.getSecond().getName().length()));
+
+        for (Pair<Integer, TagType<Recogniser<@Value ?>>> tagInfo : tagsLargestFirst)
         {
-            int iFinal = i;
-            TagType<Recogniser<@Value ?>> tag = tags.get(i);
+            TagType<Recogniser<@Value ?>> tag = tagInfo.getSecond();
             ParseProgress afterTag = pp.consumeNext(tag.getName());
             if (afterTag != null)
             {
                 @Nullable Recogniser<@Value ?> inner = tag.getInner();
                 if (inner == null)
-                    return success(new TaggedValue(i, null), afterTag);
+                    return success(new TaggedValue(tagInfo.getFirst(), null), afterTag);
                 
                 pp = afterTag.consumeNext("(");
                 if (pp == null)
@@ -39,7 +51,7 @@ public class TaggedRecogniser extends Recogniser<@Value TaggedValue>
                     ParseProgress afterBracket = succ.parseProgress.consumeNext(")");
                     if (afterBracket == null)
                         return error("Expected closing ')' after an inner value", succ.parseProgress.curCharIndex);
-                    return success(new TaggedValue(iFinal, succ.value), afterBracket);
+                    return success(new TaggedValue(tagInfo.getFirst(), succ.value), afterBracket);
                 });
                 
             }
