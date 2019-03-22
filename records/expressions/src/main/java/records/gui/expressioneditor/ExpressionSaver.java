31805d38ff10dc7ee90d81dc1ce8c5e51f0c5e60
neil <neil@twistedsquare.com>
2019-03-22 20:53:22 +0000

    Revert "Changed how we save an empty bracket to match what the tests expect."
    This reverts commit b72d291c

--- records/expressions/src/main/java/records/gui/expressioneditor/ExpressionSaver.java
+++ records/expressions/src/main/java/records/gui/expressioneditor/ExpressionSaver.java
@@ -219,7 +219,11 @@ public class ExpressionSaver extends SaverBase<Expression, ExpressionSaver, Op, 
     {
         if (content.isEmpty())
         {
-            return brackets.applyBrackets.applySingle(new InvalidIdentExpression(""));
+            @Nullable @Recorded Expression bracketedEmpty = brackets.applyBrackets.apply(new BracketContent(ImmutableList.of()));
+            if (bracketedEmpty != null)
+                return bracketedEmpty;
+            else
+                return errorDisplayerRecord.record(brackets.start, brackets.end, new InvalidOperatorExpression(ImmutableList.of()));
         }
         
         CollectedItems collectedItems = processItems(content);
