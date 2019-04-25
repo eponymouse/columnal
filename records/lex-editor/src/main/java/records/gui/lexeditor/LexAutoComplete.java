fa3ecb3eeb099c26602f41af91ab43e81f1d439c
neil <neil@twistedsquare.com>
2019-04-25 20:26:32 +0100

    Added an initial version of a better code completion dialog, not yet implemented scrolling.

--- records/lex-editor/src/main/java/records/gui/lexeditor/LexAutoComplete.java
+++ records/lex-editor/src/main/java/records/gui/lexeditor/LexAutoComplete.java
@@ -2,8 +2,16 @@ package records.gui.lexeditor;
 
 import annotation.units.CanonicalLocation;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.binding.ObjectExpression;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
+import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.ListCell;
@@ -13,11 +21,15 @@ import javafx.scene.control.Skin;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.scene.web.WebView;
 import javafx.util.Duration;
 import log.Log;
 import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import styled.StyledString;
 import threadchecker.OnThread;
 import threadchecker.Tag;
 import utility.FXPlatformRunnable;
@@ -27,8 +39,12 @@ import utility.gui.FXUtility;
 import utility.gui.GUI;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @OnThread(Tag.FXPlatform)
 public class LexAutoComplete
@@ -47,7 +63,7 @@ public class LexAutoComplete
         }));
     }
 
-    public void show(List<LexCompletion> completions)
+    public void show(ImmutableList<LexCompletion> completions)
     {
         window.setCompletions(completions);
         updateWindowPosition(completions);
@@ -58,10 +74,9 @@ public class LexAutoComplete
     private void updateWindowPosition(List<LexCompletion> completions)
     {
         Point2D caretBottom = editor.getCaretBottomOnScreen(completions.stream().mapToInt(c -> c.startPos).min().orElse(editor.getCaretPosition()));
-        ListCell listCell = (ListCell) window.listView.lookup(".list-cell");
-        double labelPad = listCell == null ? 0 : listCell.getPadding().getLeft();
+        double labelPad = window.listView.getTotalTextLeftPad() + 1;
         if (caretBottom != null && !completions.isEmpty())
-            window.show(editor, caretBottom.getX() - labelPad - 1 /* window border */, caretBottom.getY());
+            window.show(editor, caretBottom.getX() - labelPad, caretBottom.getY());
         else
             hide();
     }
@@ -80,23 +95,23 @@ public class LexAutoComplete
 
     public void down()
     {
-        int sel = window.listView.getSelectionModel().getSelectedIndex();
+        int sel = window.listView.getSelectedIndex();
         if (sel + 1 < window.listView.getItems().size())
-            window.listView.getSelectionModel().select(sel + 1);
+            window.listView.select(sel + 1);
     }
 
     public void up()
     {
-        int sel = window.listView.getSelectionModel().getSelectedIndex();
+        int sel = window.listView.getSelectedIndex();
         if (sel - 1 >= 0)
-            window.listView.getSelectionModel().select(sel - 1);
+            window.listView.select(sel - 1);
         else
-            window.listView.getSelectionModel().clearSelection();
+            window.listView.select(-1);
     }
 
     public Optional<LexCompletion> selectCompletion()
     {
-        return Optional.ofNullable(window.listView.getSelectionModel().getSelectedItem());
+        return Optional.ofNullable(window.listView.getSelectedItem());
     }
     
     public static Optional<LexCompletion> matchWordStart(String src, @CanonicalLocation int startPos, String completionText)
@@ -124,19 +139,23 @@ public class LexAutoComplete
         NO_AUTO_SELECT;
     }
 
+    // Note -- the with methods modify this class in-place.  This shouldn't be an issue as the methods are only
+    // used in a builder style.
     public static class LexCompletion
     {
         public final @CanonicalLocation int startPos;
-        public final String content;
-        public final int relativeCaretPos;
-        public final LexSelectionBehaviour selectionBehaviour;
+        public String content;
+        public StyledString display;
+        public int relativeCaretPos;
+        public LexSelectionBehaviour selectionBehaviour;
         // HTML file name (e.g. function-abs.html) and optional anchor
-        public final @Nullable Pair<String, @Nullable String> furtherDetailsURL;
+        public @Nullable Pair<String, @Nullable String> furtherDetailsURL;
 
         private LexCompletion(@CanonicalLocation int startPos, String content, int relativeCaretPos, LexSelectionBehaviour selectionBehaviour, @Nullable Pair<String, @Nullable String> furtherDetailsURL)
         {
             this.startPos = startPos;
             this.content = content;
+            this.display = StyledString.s(content);
             this.relativeCaretPos = relativeCaretPos;
             this.selectionBehaviour = selectionBehaviour;
             this.furtherDetailsURL = furtherDetailsURL;
@@ -149,29 +168,27 @@ public class LexAutoComplete
         
         public LexCompletion withReplacement(String newContent)
         {
-            return new LexCompletion(startPos, newContent, relativeCaretPos, selectionBehaviour, furtherDetailsURL);
+            this.content = newContent;
+            this.display = StyledString.s(newContent);
+            return this;
         }
         
         public LexCompletion withCaretPosAfterCompletion(int pos)
         {
-            return new LexCompletion(startPos, content, pos, selectionBehaviour, furtherDetailsURL);
+            this.relativeCaretPos = pos;
+            return this;
         }
         
         public LexCompletion withSelectionBehaviour(LexSelectionBehaviour selectionBehaviour)
         {
-            return new LexCompletion(startPos, content, relativeCaretPos, selectionBehaviour, furtherDetailsURL);
+            this.selectionBehaviour = selectionBehaviour;
+            return this;
         }
         
         public LexCompletion withFurtherDetailsURL(@Nullable String url)
         {
-            return new LexCompletion(startPos, content, relativeCaretPos, selectionBehaviour, url == null ? null : new Pair<>(url, null));
-        }
-
-        // Used by ListView to display content:
-        @Override
-        public String toString()
-        {
-            return content;
+            this.furtherDetailsURL = url == null ? null : new Pair<>(url, null);
+            return this;
         }
     }
 
@@ -179,30 +196,11 @@ public class LexAutoComplete
     public class LexAutoCompleteWindow extends PopupControl
     {
         private final Pane pane;
-        private final ListView<LexCompletion> listView;
+        private final LexCompletionList listView;
         
         public LexAutoCompleteWindow()
         {
-            this.listView = new ListView<LexCompletion>() {
-                @Override
-                @OnThread(value = Tag.FXPlatform, ignoreParent = true)
-                public void requestFocus()
-                {
-                    // Can't be focused
-                }
-            };
-            listView.getStyleClass().add("lex-completion-listview");
-            listView.setFocusTraversable(false);
-            listView.setOnMouseClicked(e -> {
-                if (e.getButton() == MouseButton.PRIMARY && e.getClickCount() == 2)
-                {
-                    @Nullable LexCompletion selected = listView.getSelectionModel().getSelectedItem();
-                    if (selected != null)
-                    {
-                        triggerCompletion.run();
-                    }
-                }
-            });
+            this.listView = new LexCompletionList();
             WebView webView = new WebView();
             webView.setFocusTraversable(false);
             webView.setPrefWidth(400.0);
@@ -214,6 +212,8 @@ public class LexAutoComplete
             setAutoHide(false);
             setHideOnEscape(false);
             setSkin(new LexAutoCompleteSkin());
+            pane.getStylesheets().add(FXUtility.getStylesheet("autocomplete.css"));
+            pane.getStyleClass().add("lex-complete-root");
             if (getScene() != null)
             {
                 getScene().addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
@@ -224,7 +224,7 @@ public class LexAutoComplete
                     }
                 });
             }
-            FXUtility.addChangeListenerPlatform(listView.getSelectionModel().selectedItemProperty(), selected -> {
+            FXUtility.addChangeListenerPlatform(listView.selectedItemProperty(), selected -> {
                 if (selected != null)
                 {
                     @Nullable Pair<String, @Nullable String> fileNameAndAnchor = selected.furtherDetailsURL;
@@ -250,20 +250,20 @@ public class LexAutoComplete
             });
         }
 
-        public void setCompletions(List<LexCompletion> completions)
+        public void setCompletions(ImmutableList<LexCompletion> completions)
         {
-            this.listView.getItems().setAll(completions);
+            this.listView.setCompletions(completions);
             if ((completions.size() == 1 && completions.get(0).selectionBehaviour == LexSelectionBehaviour.SELECT_IF_ONLY)
                 || (completions.size() >= 1 && completions.get(0).selectionBehaviour == LexSelectionBehaviour.SELECT_IF_TOP))
             {
-                listView.getSelectionModel().select(0);
+                listView.select(0);
             }
         }
 
         @OnThread(Tag.FXPlatform)
         public @Nullable String _test_getSelectedContent()
         {
-            return Utility.onNullable(listView.getSelectionModel().getSelectedItem(), l -> l.content);
+            return Utility.onNullable(listView.getSelectedItem(), l -> l.content);
         }
 
         public List<LexCompletion> _test_getShowing()
@@ -294,5 +294,142 @@ public class LexAutoComplete
         }
     }
 
-    
+    // Like a ListView, but with customisations to allow pinned "related" section
+    @OnThread(Tag.FXPlatform)
+    private class LexCompletionList extends Region
+    {
+        // The completions change all at once, so a mutable ImmutableList reference:
+        private ImmutableList<LexCompletion> curCompletions = ImmutableList.of();
+        
+        // This map's values are always inserted into the children of this region
+        private ObservableMap<LexCompletion, TextFlow> visible = FXCollections.observableMap(new IdentityHashMap<>());
+        
+        private int selectionIndex;
+        // Holds a mirror of selectionIndex:
+        private final ObjectProperty<@Nullable LexCompletion> selectedItem = new SimpleObjectProperty<>(null);
+        private int topVisibleItemIndex = 0;
+        private double topVisibleItemScroll = 0;
+        private final double ITEM_HEIGHT = 24;
+        
+        public LexCompletionList()
+        {
+            getStyleClass().add("lex-completion-list");
+            setFocusTraversable(false);
+            visible.addListener((MapChangeListener<LexCompletion, TextFlow>) (MapChangeListener.Change<? extends LexCompletion, ? extends TextFlow> c) -> {
+                if (c.wasRemoved())
+                    getChildren().remove(c.getValueRemoved());
+                if (c.wasAdded())
+                    getChildren().add(c.getValueAdded());
+            });
+            
+            setPrefWidth(300.0);
+            setMaxHeight(USE_PREF_SIZE);
+        }
+
+        @Override
+        @OnThread(value = Tag.FXPlatform, ignoreParent = true)
+        protected double computePrefHeight(double width)
+        {
+            return Math.min(curCompletions.size(), 16) * ITEM_HEIGHT;
+        }
+
+        @Override
+        @OnThread(value = Tag.FXPlatform, ignoreParent = true)
+        protected void layoutChildren()
+        {
+            double y = topVisibleItemScroll;
+            Insets insets = getInsets();
+            for (int i = topVisibleItemIndex; i < curCompletions.size() && y < getHeight(); i++, y += ITEM_HEIGHT)
+            {
+                TextFlow flow = visible.get(curCompletions.get(i));
+                if (flow != null)
+                {
+                    // Should always be non-null, but need to guard
+                    flow.resizeRelocate(insets.getLeft(), y, getWidth() - insets.getRight() - insets.getLeft(), ITEM_HEIGHT);
+                }
+            }
+        }
+        
+        private void recalculateChildren()
+        {
+            Set<LexCompletion> toKeep = Sets.newIdentityHashSet(); 
+            double y = topVisibleItemScroll;
+            for (int i = topVisibleItemIndex; i < curCompletions.size() && y < getHeight(); i++, y += ITEM_HEIGHT)
+            {
+                TextFlow item = visible.computeIfAbsent(curCompletions.get(i), this::makeFlow);
+                FXUtility.setPseudoclass(item, "selected", selectionIndex == i);
+                toKeep.add(curCompletions.get(i));
+            }
+            visible.entrySet().removeIf(e -> !toKeep.contains(e.getKey()));
+            requestLayout();
+        }
+
+        private TextFlow makeFlow(LexCompletion lexCompletion)
+        {
+            TextFlow textFlow = new TextFlow();
+            textFlow.getChildren().setAll(lexCompletion.display.toGUI());
+            textFlow.getStyleClass().add("lex-completion");
+            textFlow.setOnMouseClicked(e -> {
+                if (e.getButton() == MouseButton.PRIMARY)
+                {
+                    if (e.getClickCount() >= 1)
+                        select(Utility.findFirstIndex(curCompletions, c -> c == lexCompletion).orElse(-1));
+                    
+                    if (e.getClickCount() == 2 && selectionIndex != -1)
+                        triggerCompletion.run();
+                }
+            });
+            return textFlow;
+        }
+
+        public void setCompletions(ImmutableList<LexCompletion> completions)
+        {
+            this.curCompletions = completions;
+            // Also scroll to top:
+            topVisibleItemIndex = 0;
+            topVisibleItemScroll = 0;
+            recalculateChildren();
+            select(0);
+        }
+
+        @Override
+        @OnThread(value = Tag.FXPlatform, ignoreParent = true)
+        public void requestFocus()
+        {
+            // Can't be focused
+        }
+        
+        public void select(int itemIndex)
+        {
+            selectionIndex = Math.min(curCompletions.size() - 1, itemIndex);
+            selectedItem.setValue(selectionIndex < 0 ? null : curCompletions.get(selectionIndex));
+            // Update graphics:
+            recalculateChildren();
+        }
+        
+        public @Nullable LexCompletion getSelectedItem()
+        {
+            return selectedItem.get();
+        }
+        
+        public int getSelectedIndex()
+        {
+            return selectionIndex;
+        }
+
+        public ImmutableList<LexCompletion> getItems()
+        {
+            return curCompletions;
+        }
+
+        public ObjectExpression<@Nullable LexCompletion> selectedItemProperty()
+        {
+            return selectedItem;
+        }
+
+        public double getTotalTextLeftPad()
+        {
+            return getInsets().getLeft() + ((Region)getParent()).getInsets().getLeft() + visible.values().stream().findFirst().map(f -> f.getInsets().getLeft()).orElse(0.0);
+        }
+    }
 }
