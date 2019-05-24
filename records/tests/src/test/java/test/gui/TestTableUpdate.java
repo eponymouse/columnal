561f7e8c1fa2a28b60881822c724f2f1abcf48d9
neil <neil@twistedsquare.com>
2019-05-24 14:15:25 +0100

    Added a test which reveals the issue with deleting tables not updating the table's dependents.

--- records/tests/src/test/java/test/gui/TestTableUpdate.java
+++ records/tests/src/test/java/test/gui/TestTableUpdate.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.When;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import javafx.scene.input.KeyCode;
 import org.checkerframework.checker.initialization.qual.Initialized;
@@ -24,6 +25,7 @@ import records.error.InternalException;
 import records.error.UserException;
 import records.gui.DataCellSupplier.VersionedSTF;
 import records.gui.MainWindow.MainWindowActions;
+import records.gui.grid.RectangleBounds;
 import records.gui.table.TableDisplay;
 import records.transformations.Filter;
 import records.transformations.Sort;
@@ -34,6 +36,7 @@ import test.TestUtil;
 import test.gen.GenRandom;
 import test.gen.type.GenTypeAndValueGen;
 import test.gen.type.GenTypeAndValueGen.TypeAndValueGen;
+import test.gui.trait.ClickOnTableHeaderTrait;
 import test.gui.trait.EnterStructuredValueTrait;
 import test.gui.trait.FocusOwnerTrait;
 import test.gui.trait.ScrollToTrait;
@@ -50,12 +53,13 @@ import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 
 @RunWith(JUnitQuickcheck.class)
-public class TestTableUpdate extends FXApplicationTest implements ScrollToTrait, FocusOwnerTrait, EnterStructuredValueTrait
+public class TestTableUpdate extends FXApplicationTest implements ScrollToTrait, FocusOwnerTrait, EnterStructuredValueTrait, ClickOnTableHeaderTrait
 {    
     /**
-     * We make a two-column table, and two chained transformations of it, so that it all fits on screen
+     * We make a two-column table, and two chained identity transformations of it, so that it all fits on screen
      * (if any update problems appear, it will be most noticeable if the relevant value is already shown).
      * Then we edit the original, press enter and check:
      *  - A: the original cell has altered graphical content
@@ -71,32 +75,9 @@ public class TestTableUpdate extends FXApplicationTest implements ScrollToTrait,
             @From(GenRandom.class) Random r) throws Exception
     {
         TestUtil.printSeedOnFail(() -> {
-            TableManager dummy = new DummyManager();
-            dummy.getTypeManager()._test_copyTaggedTypesFrom(colA.getTypeManager());
-            dummy.getTypeManager()._test_copyTaggedTypesFrom(colB.getTypeManager());
             @Initialized final int tableLength = 1 + r.nextInt(20);
-            EditableRecordSet origRecordSet = new EditableRecordSet(ImmutableList.<SimulationFunction<RecordSet, EditableColumn>>of(
-                    colA.getType().makeImmediateColumn(new ColumnId("A"), Utility.<Either<String, @Value Object>>replicateM_Ex(tableLength, () -> Either.right(colA.makeValue())), colA.makeValue()),
-                    colB.getType().makeImmediateColumn(new ColumnId("B"), Utility.<Either<String, @Value Object>>replicateM_Ex(tableLength, () -> Either.right(colB.makeValue())), colB.makeValue())
-            ), () -> tableLength);
-            dummy.record(new ImmediateDataSource(dummy, new InitialLoadDetails(new TableId("Src"), CellPosition.ORIGIN.offsetByRowCols(1, 1), null), origRecordSet));
-            // Now add two transformations:
-            addTransformation(dummy, "Src", "T1", CellPosition.ORIGIN.offsetByRowCols(1, 4), r);
-            addTransformation(dummy, "T1", "T2", CellPosition.ORIGIN.offsetByRowCols(1, 7), r);
-
-            MainWindowActions details = TestUtil.openDataAsTable(windowToUse, dummy).get();
-            TestUtil.sleep(1000);
-            // First check that the data is valid to begin with:
-            List<List<@Nullable String>> origDataA = getDataViaGraphics(details, 0);
-            checkAllMatch(origDataA);
-            List<List<@Nullable String>> origDataB = getDataViaGraphics(details, 1);
-            checkAllMatch(origDataB);
-        /*
-        List<List<String>> origClipboardA = getDataViaCopy("A");
-        checkAllMatch(origDataA, origClipboardA);
-        List<List<String>> origClipboardB = getDataViaCopy("B");
-        checkAllMatch(origDataB, origClipboardB);
-        */
+            MainWindowActions details = createTables(colA, colB, r, tableLength);
+            
             int changes = 3;
             for (int i = 0; i < changes; i++)
             {
@@ -117,6 +98,65 @@ public class TestTableUpdate extends FXApplicationTest implements ScrollToTrait,
                 checkAllMatch(latestDataB);
             }
         });
+    }
+
+    @Property(trials = 5)
+    @OnThread(Tag.Simulation)
+    public void propDelete(
+        @When(seed=1L) @From(GenTypeAndValueGen.class) TypeAndValueGen colA,
+        @When(seed=1L) @From(GenTypeAndValueGen.class) TypeAndValueGen colB,
+        @When(seed=1L) @From(GenRandom.class) Random r) throws Exception
+    {
+        TestUtil.printSeedOnFail(() -> {
+            @Initialized final int tableLength = 1 + r.nextInt(20);
+            MainWindowActions details = createTables(colA, colB, r, tableLength);
+
+            // We try deleting a random table in the chain, and check that its cells are gone
+            // and any dependents also have their cells gone
+            int tableToDelete = r.nextInt(3);
+            ImmutableList<TableId> tableIds = ImmutableList.of(new TableId("Src"), new TableId("T1"), new TableId("T2"));
+            triggerTableHeaderContextMenu(details._test_getVirtualGrid(), details._test_getTableManager(), tableIds.get(tableToDelete));
+            clickOn(".id-tableDisplay-menu-delete");
+            sleep(500);
+            
+            // TODO change data before deletion
+            
+            // Now check that the cells are gone for table and dependents:
+            for (int i = tableToDelete; i < 3; i++)
+            {
+                int iFinal = i;
+                CellPosition pos = CellPosition.ORIGIN.offsetByRowCols(1 + 3, 1 + 3 * iFinal);
+                assertThrows(RuntimeException.class, () -> withItemInBounds(lookup(".document-text-field"), details._test_getVirtualGrid(), new RectangleBounds(pos, pos), (n, p) -> {}));
+            }
+            
+            // TODO put table back and check everything updates
+        });
+    }
+
+    @OnThread(Tag.Simulation)
+    private MainWindowActions createTables(@From(GenTypeAndValueGen.class) GenTypeAndValueGen.TypeAndValueGen colA, @From(GenTypeAndValueGen.class) GenTypeAndValueGen.TypeAndValueGen colB, @From(GenRandom.class) Random r, @Initialized int tableLength) throws Exception
+    {
+        TableManager dummy = new DummyManager();
+        dummy.getTypeManager()._test_copyTaggedTypesFrom(colA.getTypeManager());
+        dummy.getTypeManager()._test_copyTaggedTypesFrom(colB.getTypeManager());
+
+        EditableRecordSet origRecordSet = new EditableRecordSet(ImmutableList.<SimulationFunction<RecordSet, EditableColumn>>of(
+                colA.getType().makeImmediateColumn(new ColumnId("A"), Utility.<Either<String, @Value Object>>replicateM_Ex(tableLength, () -> Either.right(colA.makeValue())), colA.makeValue()),
+                colB.getType().makeImmediateColumn(new ColumnId("B"), Utility.<Either<String, @Value Object>>replicateM_Ex(tableLength, () -> Either.right(colB.makeValue())), colB.makeValue())
+        ), () -> tableLength);
+        dummy.record(new ImmediateDataSource(dummy, new InitialLoadDetails(new TableId("Src"), CellPosition.ORIGIN.offsetByRowCols(1, 1), null), origRecordSet));
+        // Now add two transformations:
+        addTransformation(dummy, "Src", "T1", CellPosition.ORIGIN.offsetByRowCols(1, 4), r);
+        addTransformation(dummy, "T1", "T2", CellPosition.ORIGIN.offsetByRowCols(1, 7), r);
+
+        MainWindowActions details = TestUtil.openDataAsTable(windowToUse, dummy).get();
+        TestUtil.sleep(1000);
+        // First check that the data is valid to begin with:
+        List<List<@Nullable String>> origDataA = getDataViaGraphics(details, 0);
+        checkAllMatch(origDataA);
+        List<List<@Nullable String>> origDataB = getDataViaGraphics(details, 1);
+        checkAllMatch(origDataB);
+        return details;
     }
 
     @OnThread(Tag.Any)
