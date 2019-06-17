4c1f58b20a92d665d08f23684d920a71de5f2b3e
neil <neil@twistedsquare.com>
2019-06-17 08:34:30 +0100

    Fixed the unit completion sort order and added the description as side text.

--- records/lex-editor/src/main/java/records/gui/lexeditor/UnitLexer.java
+++ records/lex-editor/src/main/java/records/gui/lexeditor/UnitLexer.java
@@ -8,6 +8,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.checkerframework.checker.i18n.qual.LocalizableKey;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import records.data.unit.SingleUnit;
 import records.data.unit.UnitManager;
 import records.gui.lexeditor.EditorLocationAndErrorRecorder.CanonicalSpan;
 import records.gui.lexeditor.Lexer.LexerResult.CaretPos;
@@ -29,6 +30,9 @@ import utility.TranslationUtility;
 
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class UnitLexer extends Lexer<UnitExpression, CodeCompletionContext>
@@ -175,9 +179,19 @@ public class UnitLexer extends Lexer<UnitExpression, CodeCompletionContext>
     
     private CodeCompletionContext makeCompletions(String stem, @CanonicalLocation int canonIndex, ChunkType curType, ChunkType preceding)
     {
-        return new CodeCompletionContext(ImmutableList.of(new LexCompletionGroup(Utility.mapListI(unitManager.getAllDeclared(), u -> {
+        ArrayList<SingleUnit> allDeclared = new ArrayList<>(unitManager.getAllDeclared());
+        Collections.<SingleUnit>sort(allDeclared, (u, v) -> u.getName().compareToIgnoreCase(v.getName()));
+        return new CodeCompletionContext(ImmutableList.of(new LexCompletionGroup(Utility.mapListI(allDeclared, u -> {
             int len = Utility.longestCommonStartIgnoringCase(u.getName(), 0, stem, 0);
-            return new LexCompletion(canonIndex, len, u.getName());
+            return new LexCompletion(canonIndex, len, u.getName()).withSideText(limit(u.getDescription()));
         }), null, 2)));
+    }
+
+    private String limit(String description)
+    {
+        if (description.length() < 20)
+            return description;
+        else
+            return description.substring(0, 19) + "\u2026";
     }
 }
