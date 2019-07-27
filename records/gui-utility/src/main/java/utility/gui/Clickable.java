748cc780c52a7b2065e0b46cc8613191bff4132a
neil <neil@twistedsquare.com>
2019-07-27 10:29:59 +0100

    Added a delay before showing the source overlay when the mouse hovers over a source table link

--- records/gui-utility/src/main/java/utility/gui/Clickable.java
+++ records/gui-utility/src/main/java/utility/gui/Clickable.java
@@ -3,12 +3,15 @@ package utility.gui;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import org.checkerframework.checker.i18n.qual.LocalizableKey;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import styled.StyledString.Style;
 import threadchecker.OnThread;
 import threadchecker.Tag;
+import utility.FXPlatformRunnable;
 import utility.FXPlatformSupplier;
 import utility.TranslationUtility;
 
@@ -36,6 +39,36 @@ public abstract class Clickable extends Style<Clickable>
     protected void setHovering(boolean hovering, Point2D screenPos)
     {
     }
+    
+    private class HoverTracker
+    {
+        private boolean hovering = false;
+        private @Nullable FXPlatformRunnable cancelHover = null;
+        
+        public void start(MouseEvent e)
+        {
+            stop(e);
+            cancelHover = FXUtility.runAfterDelay(Duration.millis(300), () -> {
+                if (!hovering)
+                {
+                    hovering = true;
+                    cancelHover = null;
+                    setHovering(true, new Point2D(e.getScreenX(), e.getScreenY()));
+                }
+            });
+        }
+        
+        public void stop(MouseEvent e)
+        {
+            hovering = false;
+            if (cancelHover != null)
+            {
+                cancelHover.run();
+                cancelHover = null;
+            }
+            setHovering(false, new Point2D(e.getScreenX(), e.getScreenY()));
+        }
+    }
 
     @Override
     @OnThread(Tag.FXPlatform)
@@ -43,13 +76,16 @@ public abstract class Clickable extends Style<Clickable>
     {
         t.getStyleClass().add("styled-text-clickable");
         t.getStyleClass().addAll(extraStyleClasses);
+        HoverTracker hoverTracker = new HoverTracker();
         t.setOnMouseClicked(e -> {
+            hoverTracker.stop(e);
             setHovering(false, new Point2D(e.getScreenX(), e.getScreenY()));
             onClick(e.getButton(), new Point2D(e.getScreenX(), e.getScreenY()));
         });
-        t.setOnMouseEntered(e -> setHovering(true, new Point2D(e.getScreenX(), e.getScreenY())));
-        t.setOnMouseMoved(e -> setHovering(true, new Point2D(e.getScreenX(), e.getScreenY())));
-        t.setOnMouseExited(e -> setHovering(false, new Point2D(e.getScreenX(), e.getScreenY())));
+        
+        t.setOnMouseEntered(e -> hoverTracker.start(e));
+        t.setOnMouseMoved(e -> hoverTracker.start(e));
+        t.setOnMouseExited(e -> hoverTracker.stop(e));
         if (tooltipKey != null)
         {
             Tooltip tooltip = new Tooltip(TranslationUtility.getString(tooltipKey));
