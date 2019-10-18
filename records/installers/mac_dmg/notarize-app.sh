ec7e95008a67324a0ffb6ea609c3408d0c8d4af1
neil <neil@twistedsquare.com>
2019-10-18 21:55:47 +0100

    Inserted the new licence agreement text, and made it appear in the Mac DMG (it already appears in the Windows MSI installer).

--- records/installers/mac_dmg/notarize-app.sh
+++ records/installers/mac_dmg/notarize-app.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-echo "Sending app to Apple to notarize.  This can take several minutes."
-xcrun altool --notarize-app --primary-bundle-id "xyz.columnal" --username "columnal@twistedsquare.com" --password "@keychain:Columnal Altool" --file "$1"
-
+echo "To send the app to Apple to notarize, run this command as a user with the right keychain:"
+echo xcrun altool --notarize-app --primary-bundle-id "xyz.columnal" --username "columnal@twistedsquare.com" --password "@keychain:Columnal Altool" --file "$1"
+echo
 echo "Make note of the UUID.  Once you get the email saying it is signed, run these two commands:"
 echo xcrun altool --notarization-info UUID -u "columnal@twistedsquare.com" --password "@keychain:Columnal Altool"
 echo xcrun stapler staple "$1"
