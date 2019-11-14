a2808b4783b8ecaf1de75ee20626c239b62919f7
neil <neil@twistedsquare.com>
2019-11-14 00:00:12 +0000

    Added another RDS test

--- records/r-interop/src/test/java/test/importExport/TestRLoadSave.java
+++ records/r-interop/src/test/java/test/importExport/TestRLoadSave.java
@@ -33,7 +33,7 @@ import static org.junit.Assert.assertEquals;
 public class TestRLoadSave
 {
     @Test
-    public void testImportRData() throws Exception
+    public void testImportRData1() throws Exception
     {
         @SuppressWarnings("nullness")
         @NonNull URL resource = getClass().getClassLoader().getResource("iris.rds");
@@ -47,6 +47,23 @@ public class TestRLoadSave
         DataTestUtil.assertValueListEqual("Row 0", ImmutableList.of(d("5.1"), d("3.5"), d("1.4"), d("0.2"), typeManager.lookupTag("setosa 3", "setosa").getRight("Tag not found").makeTag(null)), DataTestUtil.getRowVals(rs, 0));
 
         DataTestUtil.assertValueListEqual("Row 149", ImmutableList.of(d("5.9"), d("3.0"), d("5.1"), d("1.8"), typeManager.lookupTag("setosa 3", "virginica").getRight("Tag not found").makeTag(null)), DataTestUtil.getRowVals(rs, 149));
+    }
+
+    @Test
+    public void testImportRData2() throws Exception
+    {
+        @SuppressWarnings("nullness")
+        @NonNull URL resource = getClass().getClassLoader().getResource("mtcars.rds");
+        RValue loaded = RData.readRData(new File(resource.toURI()));
+        System.out.println(RData.prettyPrint(loaded));
+        TypeManager typeManager = new TypeManager(UnitManager._test_blank());
+        RecordSet rs = RData.convertRToTable(typeManager, loaded);
+
+        assertEquals(ImmutableList.of(new ColumnId("mpg"), new ColumnId("cyl"), new ColumnId("disp"), new ColumnId("hp"), new ColumnId("drat"), new ColumnId("wt"), new ColumnId("qsec"), new ColumnId("vs"), new ColumnId("am"), new ColumnId("gear"), new ColumnId("carb")), rs.getColumnIds());
+
+        DataTestUtil.assertValueListEqual("Row 0", ImmutableList.of(d("21.0"), d("6"), d("160.0"), d("110"), d("3.90"), d("2.620"), d("16.46"), d("0"), d("1"), d("4"), d("4")), DataTestUtil.getRowVals(rs, 0));
+
+        //DataTestUtil.assertValueListEqual("Row 149", ImmutableList.of(d("5.9"), d("3.0"), d("5.1"), d("1.8"), typeManager.lookupTag("setosa 3", "virginica").getRight("Tag not found").makeTag(null)), DataTestUtil.getRowVals(rs, 149));
     }
 
     private static @Value BigDecimal d(String s)
