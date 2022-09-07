e1fb9ea14559000bb0571a6880ed8e4db1e220c4
Neil Brown <>
2022-09-07 20:58:05 +0100

    Fixed an absolute type that was using the old type name in a test.

--- tests/src/test/java/test/gui/TestUnitEdit.java
+++ tests/src/test/java/test/gui/TestUnitEdit.java
@@ -88,7 +88,7 @@ public class TestUnitEdit extends FXApplicationTest implements TextFieldTrait, P
     }
 
     @OnThread(Tag.Simulation)
-    public String getUnitSrcFromFile(String fileContent) throws InternalException, records.error.UserException
+    public String getUnitSrcFromFile(String fileContent) throws InternalException, xyz.columnal.error.UserException
     {
         FileContext file = Utility.parseAsOne(fileContent, MainLexer2::new, MainParser2::new, p -> p.file());
         return Utility.getDetail(file.content().stream().filter(c -> c.ATOM(0).getText().equals("UNITS")).findFirst().orElseThrow(() -> new AssertionError("No UNITS section")).detail());
